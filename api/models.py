329f53f9c35a302c7b9352218eb2b2455b1f4ba5
Mercury_80 <harsh.gaud18@gmail.com>
2021-10-09 00:47:10 +0530

    Created custom user

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    upvote_list = models.TextField(blank=True,null=True)
+    downvote_list = models.TextField(blank=True,null=True)
+
+    def save(self):
+        user = super(User, self)
+        user.set_password(self.password)
+        user.save()
+        return user
