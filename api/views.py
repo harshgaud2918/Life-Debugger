b141d56a813ee45e74cda87015a571288b27502a
manan2110 <manan.co.in@gmail.com>
2021-10-10 16:57:29 +0530

    create problem api added

--- api/views.py
+++ api/views.py
@@ -3,13 +3,33 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import *
 from .serializers import *
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def api_problem(request):
-    problems = Problem.objects.all()
-    problems = ProblemSerializer(problems, many=True).data
-    return Response(problems)
+    if request.method == "GET":
+        problems = Problem.objects.all()
+        problems = ProblemSerializer(problems, many=True).data
+        return Response(problems)
+    else:
+        res = {}
+        res["description"] = request.POST["description"]
+        res["category"] = request.POST["category"]
+        res["location"] = request.POST["location"]
+        res["upvote_count"] = request.POST["upvote_count"]
+        res["downvote_count"] = request.POST["downvote_count"]
+        res["severity"] = request.POST["severity"]
+        res["user"] = request.POST["user"]
+        serializer = ProblemSerializer(data=res)
+        if serializer.is_valid():
+            serializer.save()
+            res = serializer.data
+            res["message"] = "Your Problem is created. You will be notified soon."
+            return Response(res)
+        res = serializer.errors
+        res["message"] = "There was a problem while creating the problems"
+        return Response(res, status.HTTP_400_BAD_REQUEST)
